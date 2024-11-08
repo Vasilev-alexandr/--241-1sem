2e745d36abf95227294a389ef2a5d35bf6233953
Vasilev-alexandr <vasludvladic@mail.ru>
2024-11-08 16:51:42 +0300

    лишнее

--- List/List/main.cpp
+++ List/List/main.cpp
@@ -9,60 +9,42 @@ int main()
 
     mynamespace::CircularLinkedList<int> list = { 1, 2, 3, 4 };
 
-    std::cout << "»сходный список: " << list.toString() << std::endl;
+    std::cout << "Исходный список: " << list << std::endl;
 
-    std::cout << "¬ведите числа дл¤ добавлени¤ (закончите ввод нечисловым символом): ";
+    std::cout << "Введите числа для добавления (закончите ввод нечисловым символом): ";
     std::cin >> list;
 
-    std::cout << "—писок после добавлени¤: " << list.toString() << std::endl;
+    std::cout << "Список после добавления: " << list << std::endl;
 
-    std::cout << "¬ведите число дл¤ удалени¤: ";
-
+    std::cout << "Введите число для удаления: ";
     int valueToRemove;
-
     std::cin >> valueToRemove;
-
     list.remove(valueToRemove);
+    std::cout << "Список после удаления: " << list << std::endl;
 
-    std::cout << "—писок после удалени¤: " << list.toString() << std::endl;
-
-    std::cout << "¬ведите число дл¤ вставки: ";
-
+    std::cout << "Введите число для вставки: ";
     int valueToInsert;
     std::cin >> valueToInsert;
-
-    std::cout << "¬ведите число после которого вставл¤ть: ";
+    std::cout << "Введите число после которого вставлять: ";
     int valueToInsertAfter;
-
     std::cin >> valueToInsertAfter;
-
     list.insertAfter(valueToInsertAfter, valueToInsert);
+    std::cout << "Список после вставки: " << list << std::endl;
 
-    std::cout << "—писок после вставки: " << list.toString() << std::endl;
-
-    std::cout << "¬ведите число, после которого удалить: ";
+    std::cout << "Введите число, после которого удалить: ";
     int valueToRemoveAfter;
-
-
     std::cin >> valueToRemoveAfter;
-
     list.removeAfter(valueToRemoveAfter);
+    std::cout << "Список после удаления после: " << list << std::endl;
 
-    std::cout << "—писок после удалени¤ после: " << list.toString() << std::endl;
-
-    std::cout << "¬ведите число дл¤ поиска: ";
-
+    std::cout << "Введите число для поиска: ";
     int valueToFind;
-
     std::cin >> valueToFind;
-
-    if (list.find(valueToFind))
-    {
-        std::cout << "„исло найдено в списке." << std::endl;
+    if (list.find(valueToFind)) {
+        std::cout << "Число найдено в списке." << std::endl;
     }
-    else
-    {
-        std::cout << "„исло не найдено в списке." << std::endl;
+    else {
+        std::cout << "Число не найдено в списке." << std::endl;
     }
 
     return 0;
